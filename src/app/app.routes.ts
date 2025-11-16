825534c009f30bb302ce2b6b9175d95a0cdcd1b7
Jhan7two <jhancarlo190@gmail.com>
2025-11-15 22:48:55 -0400

    Update routing to include login and redirect to login page
    - Added a login route with LoginComponent and loginGuard for authentication.
    - Updated existing routes to include authGuard for protected components.
    - Redirected the default path to the login page instead of the panel.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,24 +3,43 @@ import { PanelComponent } from './pages/panel/panel.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { GestionContenidoComponent } from './pages/gestion-contenido/gestion-contenido.component';
 import { GestionAvanceComponent } from './pages/gestion-avance/gestion-avance.component';
+import { LoginComponent } from './auth/login/login.component';
+import { loginGuard } from './auth/login.guard';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { 
+    path: 'login', 
+    component: LoginComponent,
+    canActivate: [loginGuard]
+  },
+  { 
     path: 'panel', 
     component: PanelComponent,
+    canActivate: [authGuard]
   },
   { 
     path: 'usuarios', 
     component: UsuariosComponent,
+    canActivate: [authGuard]
   },
   { 
     path: 'gestion-contenido', 
     component: GestionContenidoComponent,
+    canActivate: [authGuard]
   },
   { 
     path: 'gestion-avance', 
     component: GestionAvanceComponent,
+    canActivate: [authGuard]
   },
   { 
-    path: '**', redirectTo: 'panel' }
+    path: '', 
+    redirectTo: '/login', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: '**', 
+    redirectTo: '/login' 
+  }
 ];
